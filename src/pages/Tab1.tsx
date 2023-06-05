4b971e939744c189e26f998ad08975249521a0fc
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-06-05 16:28:41 +0500

    feat: actionsheet

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -10,7 +10,29 @@ import {
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 import ShowHelloToast from "../components/ToastComp";
+import { ActionSheet, ActionSheetButtonStyle } from "@capacitor/action-sheet";
+
 const Tab1: React.FC = () => {
+  const showActions = async () => {
+    const result = await ActionSheet.showActions({
+      title: "Photo Options",
+      message: "Select an option to perform",
+      options: [
+        {
+          title: "Upload",
+        },
+        {
+          title: "Share",
+        },
+        {
+          title: "Remove",
+          style: ActionSheetButtonStyle.Destructive,
+        },
+      ],
+    });
+
+    console.log("Action Sheet result:", result);
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -26,6 +48,7 @@ const Tab1: React.FC = () => {
             <IonButton onClick={() => ShowHelloToast("ok Good Work")}>
               Button
             </IonButton>
+            <IonButton onClick={showActions}>showActions Button</IonButton>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
